25b6c53891eb500bb7c65ec1d16142bbb647be71
Charles Vu <charles.a.vu@gmail.com>
2016-11-19 16:50:20 +0000

    Added empty project

---
+++ HuxleySwift/HuxleySwift.h
@@ -0,0 +1,19 @@
+//
+//  HuxleySwift.h
+//  HuxleySwift
+//
+//  Created by charles on 19/11/2016.
+//  Copyright © 2016 charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HuxleySwift.
+FOUNDATION_EXPORT double HuxleySwiftVersionNumber;
+
+//! Project version string for HuxleySwift.
+FOUNDATION_EXPORT const unsigned char HuxleySwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HuxleySwift/PublicHeader.h>
+
+
