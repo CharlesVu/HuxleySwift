a770c433730e16b1f905f4fd109e2abf7d81c076
Charles Vu <753278+CharlesVu@users.noreply.github.com>
2019-06-04 12:56:34 +0100

    reverted trainServices

--- Sources/HuxleySwift/TrainServices.swift
+++ Sources/HuxleySwift/TrainServices.swift
@@ -10,16 +10,16 @@ import Foundation
 
 public class TrainServices : Codable {
     public private(set) var subsequentCallingPoints: [SubsequentCallingPoint] = []
-    public private(set) var origin: Origin
-    public private(set) var destination: Destination
-    
-    public let currentOrigins: String
-    public let currentDestinations: String
-    public let rsid: String
-    public let sta: String
-    public let eta: String
-    public let std: String
-    public let etd: String
+    public private(set) var origin: Origin?
+    public private(set) var destination: Destination?
+
+    public let currentOrigins: String?
+    public let currentDestinations: String?
+    public let rsid: String?
+    public let sta: String?
+    public let eta: String?
+    public let std: String?
+    public let etd: String?
 
     public let platform: String?
     public let `operator`: String?
@@ -32,11 +32,10 @@ public class TrainServices : Codable {
     public let length: Int
     public let detachFront: Bool
     public let isReverseFormation: Bool
-    public let cancelReason: String
-    public let delayReason: String
-    public let serviceID: String
-    public let serviceIdPercentEncoded: String
-    public let serviceIdGuid: String
-    public let serviceIdUrlSafe: String
-    public let adhocAlerts: String
-}
+    public let cancelReason: String?
+    public let delayReason: String?
+    public let serviceID: String?
+    public let serviceIdPercentEncoded: String?
+    public let serviceIdGuid: String?
+    public let serviceIdUrlSafe: String?
+    public let adhocAlerts: String?}
